e2194b993cf716db1f188b5c4ee63c8f745b983b
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2021-03-19 22:14:14 -0300

    Se realizaron las practicas 2 y 3

---
+++ Practica3(Pintura)/js/index.js
@@ -0,0 +1,19 @@
+let btnCalcular = document.getElementById("btnCalcular");
+
+
+btnCalcular.addEventListener('click', calcularCosto);
+
+
+function calcularCosto() {
+    let costoMateriales = Number(document.getElementById("costoMaterial").value);
+    let anchoPared = Number(document.getElementById("AnchoPared").value);
+    let altoPared = Number (document.getElementById("AltoPared").value);
+
+    let resultado = altoPared * anchoPared * costoMateriales;
+
+    let containerResultado = document.getElementById("resultado");
+
+    containerResultado.innerHTML=`
+    <em>El resultado es:</em> ${resultado}
+    `;
+}
