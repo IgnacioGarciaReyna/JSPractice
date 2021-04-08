cfda4f2a9e31eca930613ee3f99fccd808bdc880
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2021-04-07 21:10:52 -0300

    Se realizó la práctica 2 de Condicionales Anidadas con Else If

--- CondicionalesAnidadas/Práctica2/js/index.js
+++ CondicionalesAnidadas/Práctica2/js/index.js
@@ -1,5 +1,5 @@
 const btnCalculardigitos = document.getElementById('btnCalcularDigitos');
-btnCalculardigitos.addEventListener('click', calcularDigitos);
+btnCalculardigitos.addEventListener('click', calcularDigitosOL);
 
 function calcularDigitos() {
     const inpNumero1 = Number(document.getElementById('inpNumero1').value);
@@ -16,6 +16,24 @@ function calcularDigitos() {
         resultado = "El número es de mas de 3 dígitos"; 
     }
     
+
+    const containerResult = document.getElementById('containerResult');
+    containerResult.innerText = resultado;
+}
+
+function calcularDigitosOL () {
+    const inpNumero1 = Number(document.getElementById('inpNumero1').value);
+    let resultado = "";
+
+    if (inpNumero1 >=99 && inpNumero1 <= 999) {
+        resultado = "El número cuenta con 3 dígitos";
+    } else if (inpNumero1 > 9 && inpNumero1 <= 99) {
+        resultado = "El número cuenta con 2 dígitos";
+    } else if (inpNumero1 > 0 && inpNumero1 <= 9) {
+        resultado = "El número cuenta con 1 dígitos";
+    } else {
+        resultado = "El número cuenta con más de 3 dígitos";
+    }
 
     const containerResult = document.getElementById('containerResult');
     containerResult.innerText = resultado;
