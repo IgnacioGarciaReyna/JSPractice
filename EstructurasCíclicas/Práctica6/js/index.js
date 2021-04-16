be3f6c532665aaf1b523f71b5ffddba9dd60e599
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2021-04-16 20:02:28 -0300

    Se realizó la práctica 6 de Estructuras Cíclicas

---
+++ EstructurasCíclicas/Práctica6/js/index.js
@@ -0,0 +1,14 @@
+const btnInitApp = document.getElementById('btnInitApp');
+
+btnInitApp.addEventListener('click', imprimirNumeros);
+
+function imprimirNumeros() {
+    let resultadoAcumulado = "";
+    for (let i=0; i<=100; i++) {
+        if(i % 2 == 0) {
+            resultadoAcumulado += `<li class="list-group-item">El número ${i} es par.</li>`;
+        } 
+    }
+    const resultContainer = document.getElementById('resultContainer');
+    resultContainer.innerHTML = resultadoAcumulado;
+}
