efa39bdee31536b16817226062d3a23ca1e5843b
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2021-05-05 00:16:29 -0300

    Se realizó la Práctica 14 de Arreglos (Ejercicio9)

---
+++ Arreglos/Práctica14/js/index.js
@@ -0,0 +1,40 @@
+let btnMostrarArray = document.getElementById('btnMostrarArray');
+btnMostrarArray.addEventListener('click', crearArray);
+
+let arreglo1 = [];
+let arreglo2 = [];
+
+
+function crearArray () {
+    for(let i = 0; i < 30; i++) {
+        numero = Math.floor(Math.random() * (300 - 101)) + 101;
+        arreglo1[i] = numero;
+        let isInpar = validarNumeroPrimo(numero);
+        if (isInpar == true) {
+            arreglo2.push(numero);
+        }
+    }
+
+    let container1 = document.getElementById('container1');
+    let container2 = document.getElementById('container2');
+    container1.innerHTML = `<div class="alert alert-success">El array inicial es: [${arreglo1}]</div>`;
+    container2.innerHTML = `<div class="alert alert-success">Los números impares son: [${arreglo2}]</div>`;
+}
+
+function validarNumeroPrimo (numero) {
+    if (numero % 2 == 0) {
+        return false;
+    }
+    return true;
+}
+
+
+    
+    
+    
+
+
+
+
+    
+    
